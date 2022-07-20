595ab37d804a88fb13a3f0faf520d5a78a388350
sinkovit <sinkovit@sdsc.edu>
2022-07-20 12:26:00 -0700

    Cleans up sed example

--- Linux-text-tools/Sed/sed-cmds.sh
+++ Linux-text-tools/Sed/sed-cmds.sh
@@ -8,7 +8,9 @@ sed -n '3,5'p file2.txt
 sed -n '2~3'p file2.txt
 
 head -2 file1.txt > file3.txt
-cat file3.txt
-sed s'/pear/XXXX/' file3.txt
 sed s'/pear/XXXX/' file3.txt > file3.txt
+cat file3.txt
+
+head -2 file1.txt > file3.txt
+sed -i s'/pear/XXXX/' file3.txt
 cat file3.txt
